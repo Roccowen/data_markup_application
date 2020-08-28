c4167a05ea21948b03c4d76b7282257c18cca294
Roccowen <mirvodartem@gmail.com>
2020-08-29 01:41:01 +0600

    add main

---
+++ WindowsFormsApplication347/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication347
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Enabled = false;
+            this.PictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(694, 488);
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.PictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.PictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.selectImage_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(211, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(130, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Previous";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(637, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Undo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.PictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 491);
+            this.panel1.TabIndex = 6;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 543);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Markup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApplication347/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication347
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Point MousePos1;
+        private Point MousePos2;
+
+        string[] FramesPaths;
+        string CurrentFrame;
+        int Position;
+        string FormatsPattern = @"(.*\.jpg|.*\.png|.*\.jpeg|.*\.bmp)";
+        Stack<Image> Changes = new Stack<Image>(5);
+
+
+        Rectangle GetRect(Point p1, Point p2)
+        {
+            var x1 = Math.Min(p1.X, p2.X);
+            var x2 = Math.Max(p1.X, p2.X);
+            var y1 = Math.Min(p1.Y, p2.Y);
+            var y2 = Math.Max(p1.Y, p2.Y);
+            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+        }
+
+        private void selectImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Filter = "Файлы изображений|*.jpg;*.png;*.jpeg;*.bmp";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {                        
+                        PictureBox1.Image = Image.FromFile(ofd.FileName);
+                        CurrentFrame = Path.GetFullPath(ofd.FileName);
+                        FramesPaths = Directory.GetFiles(Path.GetDirectoryName(ofd.FileName))
+                            .Where(n => Regex.IsMatch(n, FormatsPattern))
+                            .OrderBy(n => n)
+                            .ToArray();
+                        Position = Array.IndexOf(FramesPaths, CurrentFrame);
+                    }
+                }
+            }
+            catch 
+            {
+                FramesPaths = null;
+                CurrentFrame = null;
+                Position = 0;
+            }
+            if (PictureBox1.Image != null)
+            {
+                PictureBox1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }       
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MousePos2 = e.Location;
+                PictureBox1.Invalidate();
+            }
+            else
+            {
+                MousePos1 = MousePos2 = e.Location;
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (MousePos1 != MousePos2)
+            {
+                var rect = GetRect(MousePos1, MousePos2);
+
+                Pen pen = new Pen(Color.Red, 3);
+                PictureBox1.CreateGraphics().DrawRectangle(pen, rect);
+                using (var img = Graphics.FromImage(PictureBox1.Image))
+                {
+                    img.DrawRectangle(pen, rect);
+                }
+                // rect.X,  rect.Y, rect.Height, rect.Width      
+            }
+            PictureBox1.Invalidate();
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (MousePos1 != MousePos2)
+                ControlPaint.DrawFocusRectangle(e.Graphics, GetRect(MousePos1, MousePos2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            Position += 1;
+            if (Position >= FramesPaths.Length)
+                Position = 0;
+            PictureBox1.Image.Dispose();
+            PictureBox1.Image = Image.FromFile(FramesPaths[Position]);                       
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Position -= 1;
+            if (Position <= 0)
+                Position = FramesPaths.Length - 1;
+            PictureBox1.Image.Dispose();
+            PictureBox1.Image = Image.FromFile(FramesPaths[Position]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Writer()
+        {
+            string markedData = CurrentFrame.Substring(0, CurrentFrame.LastIndexOf('/')) + "marked.txt";
+            var fs = new FileStream(markedData, FileMode.Append);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            if (this.PictureBox1.Image != null)
+            {
+                Size picSize = this.PictureBox1.Image.Size;
+                Size panSize = this.panel1.Size;
+                if (picSize.Height < panSize.Height)
+                {
+                    this.PictureBox1.Location = new Point(this.PictureBox1.Location.X, (panSize.Height - picSize.Height) / 2);
+                }
+                else
+                {
+                    this.PictureBox1.Location = new Point(this.PictureBox1.Location.X, 0);
+                }
+                if (picSize.Width < panSize.Width)
+                {
+                    this.PictureBox1.Location = new Point((panSize.Width - picSize.Width) / 2, this.PictureBox1.Location.Y);
+                }
+                else
+                {
+                    this.PictureBox1.Location = new Point(0, this.PictureBox1.Location.Y);
+                }
+            }
+        }
+    }
+}
